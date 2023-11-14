f2dc2404d5e418f5baaad1bdb828aa6b0f38de23
Phelipe <phelipemanoel@gmail.com>
2023-11-14 06:51:46 -0300

    header pre-ok

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('menuTrigger', [
+      state('open', style({
+        left: '0'
+      })),
+      state('closed', style({
+        left: '-80%'
+      })),
+      transition('closed => open', [
+        animate('300ms ease-in')
+      ]),
+      transition('open => closed', [
+        animate('300ms ease-out')
+      ])
+    ])
+  ],
 })
 export class HeaderComponent {
+  menuOpen: boolean = false;
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
 }
